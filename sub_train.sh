c9252bc8858b0e675f93d0a7a76b292f75d2b0a0
Stephen Woloszynek <sw424@proteusi01.cm.cluster>
2018-02-09 15:13:57 -0500

    changed sub_train for stateful

--- sub_train.sh
+++ sub_train.sh
@@ -4,6 +4,7 @@
 #$ -cwd
 #$ -M sw424@drexel.edu
 #$ -l h_rt=48:00:00
+#$ -l ua=haswell
 #$ -P rosenPrj
 #$ -pe shm 16
 #$ -l mem_free=3G
@@ -16,13 +17,14 @@ module load proteus
 module load sge/univa
 module load gcc/4.8.1
 module load python/intelpython/3.5.3
+#module load python/intelpython/3.6.3
 
 k=4
 class=lake
 USERNAME=sw424
 coding=1
 
-SCRATCH=/scratch/$USERNAME/train_$class
+SCRATCH=/scratch/$USERNAME/train_${class}_${coding}_stateful_128b_clip
 SCRATCH_OUT=$SCRATCH/out
 SCRATCH_DATA=$SCRATCH/data
 SCRATCH_KMERS=$SCRATCH_DATA/kmers_$k
@@ -38,7 +40,8 @@ KMERS=/home/$USERNAME/earth/data/kmers_$k
 OUT=/home/$USERNAME/earth/out
 
 py=/mnt/HA/opt/python/intel/2017/intelpython3/bin/python3.5
-script=7_model.py
+#py=/mnt/HA/opt/python/intel/2018.1.023/intelpython3/bin/python3
+script=7_model_stateful.py
 labels=/home/$USERNAME/earth/data/labels.csv
 
 cp -r $KMERS/* $SCRATCH_KMERS/
